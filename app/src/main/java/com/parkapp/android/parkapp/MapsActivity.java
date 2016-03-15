f8458b07a5ea987d2aff659d2c88aaf17d70edac
Levi Orta <levi.orta@gmail.com>
2016-03-15 20:17:56 +0100

    Markers are updated in realtime with async calls in the background

--- app/src/main/java/com/parkapp/android/parkapp/MapsActivity.java
+++ app/src/main/java/com/parkapp/android/parkapp/MapsActivity.java
@@ -1,6 +1,6 @@
 package com.parkapp.android.parkapp;
 
-import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,29 +13,25 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, AsyncResponse {
-    SensorsAsyncTask sensorsAsyncTask = new SensorsAsyncTask();
     private GoogleMap mMap;
+    private Map<String,Marker> sensors = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //this to set delegate/listener back to this class
-        sensorsAsyncTask.delegate = this;
-
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -46,86 +42,87 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        CameraPosition camPos = new CameraPosition.Builder()
+                .target(new LatLng(36.535736, -6.300415))
+                .zoom(16)
+                .build();
+        CameraUpdate camUpd = CameraUpdateFactory.newCameraPosition(camPos);
+        mMap.animateCamera(camUpd);
 
-        //execute the async task
-        sensorsAsyncTask.execute();
+        callAsynchronousTask();
+    }
+
+    public void callAsynchronousTask() {
+        final Handler handler = new Handler();
+        Timer timer = new Timer();
+        TimerTask doAsynchronousTask = new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    public void run() {
+                        try {
+                            executeSensorsAsyncTask("http://10.0.2.2/sensors.php");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        };
+
+        executeSensorsAsyncTask("http://10.0.2.2/sensors.php"); //first execution
+        timer.schedule(doAsynchronousTask, 15000, 15000); //execute in every 15000 ms
+    }
+
+    public void executeSensorsAsyncTask(String path) {
+        SensorsAsyncTask sensorsAsyncTask = new SensorsAsyncTask();
+        //this to set delegate/listener back to this class
+        sensorsAsyncTask.delegate = this;
+        sensorsAsyncTask.execute(path);
     }
 
     //this override the implemented method from AsyncResponse
     public void processFinish(String output) {
-        //Here you will receive the result fired from async class
+        //Here you will receive the result fired from sensorsAsyncTask class
         //of onPostExecute(result) method.
 
         try {
             JSONArray sensorsArray = new JSONArray(output);
 
-            LatLng coordinates = new LatLng(-34, 151);
-            for (int i = 0, size = sensorsArray.length(); i < size; i++)
-            {
-                JSONObject sensor = sensorsArray.getJSONObject(i);
-                Double lat = Double.parseDouble(sensor.getString("lat"));
-                Double lng = Double.parseDouble(sensor.getString("lon"));
-                Boolean status = "1".equals(sensor.getString("status"));
-                coordinates = new LatLng(lat, lng);
-                if (status) {
-                    mMap.addMarker(new MarkerOptions().position(coordinates).icon(BitmapDescriptorFactory
-                            .defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
-                } else {
-                    mMap.addMarker(new MarkerOptions().position(coordinates).icon(BitmapDescriptorFactory
-                            .defaultMarker(BitmapDescriptorFactory.HUE_RED)));
-                }
+            for (int i = 0, size = sensorsArray.length(); i < size; i++) {
+                addSensorToMap(sensorsArray.getJSONObject(i));
             }
-
-            CameraPosition camPos = new CameraPosition.Builder()
-                    .target(coordinates)   //Centramos el mapa en Madrid
-                    .zoom(15)         //Establecemos el zoom en 15
-                    .build();
-
-            CameraUpdate camUpd =
-                    CameraUpdateFactory.newCameraPosition(camPos);
-            mMap.animateCamera(camUpd);
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
-    public class SensorsAsyncTask extends AsyncTask<String, String, String> {
-        public AsyncResponse delegate = null;
+    public void addSensorToMap(JSONObject sensor) {
+        try {
+            String sensorId = sensor.getString("sensorID");
+            Double lat = Double.parseDouble(sensor.getString("lat"));
+            Double lng = Double.parseDouble(sensor.getString("lon"));
+            Boolean free = "1".equals(sensor.getString("status"));
 
-        HttpURLConnection urlConnection;
-
-        @Override
-        protected String doInBackground(String... args) {
-            StringBuilder result = new StringBuilder();
-
-            try {
-                URL url = new URL("http://10.0.2.2/sensors.php");
-                urlConnection = (HttpURLConnection) url.openConnection();
-                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-
-                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    result.append(line);
-                }
-
-            }catch( Exception e) {
-                e.printStackTrace();
+            if(sensors.containsKey(sensorId)){
+                Marker marker = sensors.get(sensorId);
+                marker.remove();
+                sensors.remove(sensorId);
             }
-            finally {
-                urlConnection.disconnect();
+            MarkerOptions markerOptions = new MarkerOptions();
+
+            LatLng coordinates = new LatLng(lat, lng);
+            if (free) {
+                markerOptions.position(coordinates).icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+            } else {
+                markerOptions.position(coordinates).icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_RED));
             }
 
-            return result.toString();
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            //Do something with the JSON string
-            delegate.processFinish(result);
+            sensors.put(sensorId, mMap.addMarker(markerOptions));
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
     }
-
-
 }
---
+++ app/src/main/java/com/parkapp/android/parkapp/SensorsAsyncTask.java
@@ -0,0 +1,48 @@
+package com.parkapp.android.parkapp;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class SensorsAsyncTask extends AsyncTask<String, String, String> {
+    public AsyncResponse delegate = null;
+
+    HttpURLConnection urlConnection;
+
+    @Override
+    protected String doInBackground(String... args) {
+        StringBuilder result = new StringBuilder();
+
+        try {
+            //URL url = new URL("http://10.0.2.2/sensors.php");
+            URL url = new URL(args[0]);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+
+        }catch( Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            urlConnection.disconnect();
+        }
+
+        return result.toString();
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        delegate.processFinish(result);
+    }
+}
